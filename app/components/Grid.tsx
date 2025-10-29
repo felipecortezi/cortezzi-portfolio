447e2bcb8eb849ccf5438cb4d79b4d575862187c
felipecortezi <contato@cortezzi.com.br>
2025-10-29 10:21:17 -0300

    Update Grid.tsx

--- app/components/Grid.tsx
+++ app/components/Grid.tsx
@@ -19,10 +19,10 @@ export default function Grid() {
                 </div>
                 <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     <Job link="https://youtu.be/O54EjAK1ATY" title="Pré Wedding - Bruna & Pedro" description="Bruna & Pedro • 2025 • Captação & Edição" image={preWedding}/>
-                    <Job link="https://youtu.be/Q6MU7rZ9OMY" title="Institucional Fibraw" description="Cliente • 2025 • Motion" image={fibraw}/>
+                    <Job link="https://youtu.be/Q6MU7rZ9OMY" title="Institucional Fibraw" description="Fibraw • 2025 • Captação & Edição" image={fibraw}/>
                     <Job link="https://youtu.be/9vBLhxoOzxE" title="AgriShow 2025" description="Canal Rural • 2025 • Captação" image={agrishow}/>
-                    <Job link="https://youtu.be/o37egYio23M" title="Showreel: Renan Mazer" description="Renan Mazer • 2025 • Captação" image={renanMazer}/>
-                    <Job link="https://youtu.be/9xWoFBFOPAE" title="Mini Documentário: O Novo Kids" description="Ágape Igreja Missionária • 2025 • Captação" image={novokids}/>
+                    <Job link="https://youtu.be/o37egYio23M" title="Showreel: Renan Mazer" description="Renan Mazer • 2025 • Captação & Edição" image={renanMazer}/>
+                    <Job link="https://youtu.be/9xWoFBFOPAE" title="Mini Documentário: O Novo Kids" description="Ágape Igreja Missionária • 2025 • Captação & Edição" image={novokids}/>
                     <Job link="https://youtu.be/uKjzFb4R8qI" title="Institucional Artis Studio" description="Artis Studio • 2025 • Motion Design" image={artis}/>
                 </div>
             </div>
