a60f8a8415b335d89a47ed5c08a8a876a9e0afa0
felipecortezi <contato@cortezzi.com.br>
2025-11-06 21:03:35 -0300

    Update sanity.queries.ts

--- lib/sanity.queries.ts
+++ lib/sanity.queries.ts
@@ -1,17 +1,24 @@
-export const allProjectsQuery = `
-*[_type == "project"] | order(coalesce(date, _updatedAt) desc){
+// 6 mais recentes (usa date quando existir; senão updatedAt/createdAt)
+export const sixLatestProjectsQuery = `
+*[_type == "project"] 
+| order(coalesce(date, _updatedAt, _createdAt) desc)[0...6]{
   _id, title, description, link, embedUrl, featured,
   "thumbUrl": thumb.asset->url
-}`;
+}
+`;
+
+export const allProjectsQuery = `
+*[_type == "project"] 
+| order(coalesce(date, _updatedAt, _createdAt) desc){
+  _id, title, description, link, embedUrl, featured,
+  "thumbUrl": thumb.asset->url
+}
+`;
 
 export const featuredProjectQuery = `
-*[_type == "project" && featured == true][0]{
-  _id, title, description, link, embedUrl,
+*[_type == "project" && featured == true]
+| order(coalesce(date, _updatedAt, _createdAt) desc)[0]{
+  _id, title, description, link, embedUrl, featured,
   "thumbUrl": thumb.asset->url
-}`;
-
-export const sixLatestProjectsQuery = `
-*[_type == "project"] | order(coalesce(date, _updatedAt) desc)[0...6]{
-  _id, title, description, link, embedUrl,
-  "thumbUrl": thumb.asset->url
-}`;
+}
+`;
